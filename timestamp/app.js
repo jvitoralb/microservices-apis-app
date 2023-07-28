fb1d0050286c344847c0873f567086f96fce1010
jvitoralb <jvvalb@gmail.com>
2023-07-28 16:13:07 -0300

    update: isolated timestamp service

---
+++ timestamp/app.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import frontend from './lib/path.js';
+import timestamp from './api/routes.js';
+import notFound from './lib/notFound.js';
+import { handleError } from './api/middleware.js';
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use(express.static(`${frontend}`));
+
+app.use('/timestamp', timestamp);
+
+app.use(notFound);
+app.use(handleError);
+
+export default app;
