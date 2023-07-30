fdcdf1a354ec8a5eb0de039a233ec4c1051b3ac2
jvitoralb <jvvalb@gmail.com>
2023-07-30 12:06:25 -0300

    update: fix services proxy

--- gateway/api/index.js
+++ gateway/api/index.js
@@ -7,11 +7,11 @@ const gateway = (app) => {
         res.sendFile(`${frontend}/public/index.html`);
     });
 
-    app.use('/timestamp', proxy('http://localhost:3001/timestamp'));
-    app.use('/header-parser', proxy('http://localhost:3002/header-parser'));
-    app.use('/url-shortener', proxy('http://localhost:3003/url-shortener'));
-    app.use('/exercise-tracker', proxy('http://localhost:3004/exercise-tracker'));
-    app.use('/file-metadata', proxy('http://localhost:3005/file-metadata'));
+    app.use('/timestamp', proxy('http://localhost:3001'));
+    app.use('/header-parser', proxy('http://localhost:3002'));
+    app.use('/url-shortener', proxy('http://localhost:3003'));
+    app.use('/exercise-tracker', proxy('http://localhost:3004'));
+    app.use('/file-metadata', proxy('http://localhost:3005'));
 }
 
 export default gateway;
