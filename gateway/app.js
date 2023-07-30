9e4e3215056576a49e4a179aa63506fc6e8de407
jvitoralb <jvvalb@gmail.com>
2023-07-30 12:18:33 -0300

    update: rmv conflicting middlewares

--- gateway/app.js
+++ gateway/app.js
@@ -8,8 +8,6 @@ import { handleError, notFound } from './api/middlewares.js';
 const app = express();
 
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({extended: true}));
 app.use(express.static(`${frontend}`));
 
 gateway(app);
